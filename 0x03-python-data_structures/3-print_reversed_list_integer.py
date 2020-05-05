3e862abf5804117ed1a50ed75b0907b318a1b76e
Corbin Enterline <1692@holbertonschool.com>
2020-05-05 22:11:43 +0000

    Edge case for empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    x = len(my_list)
-    for a in range(x, 0, -1):
-        print("{:d}".format(a))
+    if my_list:
+        x = len(my_list)
+        if my_list == NULL:
+            exit()
+        for a in range(x, 0, -1):
+            print("{:d}".format(a))
