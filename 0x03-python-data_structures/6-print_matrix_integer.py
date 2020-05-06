3dbcaf4cd4f0c51fb31cbe88064547299b2442c0
Corbin Enterline <1692@holbertonschool.com>
2020-05-06 01:52:31 +0000

    Rewrote to use .format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for s in matrix:
-        print(*s)
+    if matrix:
+        for a in range(len(matrix)):
+            for b in range(len(matrix[a])):
+                if b + 1 == len(matrix[a]):
+                    print("{:d} ".format(matrix[a][b]))
+                else:
+                    print("{:d}".format(matrix[a][b]), end=' ')
