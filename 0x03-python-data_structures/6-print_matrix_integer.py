26cca3aada1c7be598761e22608b7f0c433561df
Corbin Enterline <1692@holbertonschool.com>
2020-05-06 01:59:32 +0000

    Fixed empty nested list

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
+    if matrix[0]:
         for a in range(len(matrix)):
             for b in range(len(matrix[a])):
                 if b + 1 == len(matrix[a]):
-                    print("{:d} ".format(matrix[a][b]))
+                    print("{:d}".format(matrix[a][b]))
                 else:
                     print("{:d}".format(matrix[a][b]), end=' ')
+    else:
+        print()
