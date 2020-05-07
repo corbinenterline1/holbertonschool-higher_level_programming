a590a9dad037dffb67412d0293ba588aad38c0d0
Corbin Enterline <1692@holbertonschool.com>
2020-05-07 17:37:51 +0000

    Accounted for empty list

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if not my_list:
+        return (0);
     scorelist = [(x * y) for x, y in my_list]
     score = sum(scorelist)
     weightlist = [i[1] for i in my_list]
