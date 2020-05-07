9077ff57662e120e16525170c31ad7d31fa64c21
Corbin Enterline <1692@holbertonschool.com>
2020-05-07 17:49:35 +0000

    Account for empty dictionary

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    doubled = a_dictionary.copy()
-    for a in doubled:
-        doubled[a] *= 2
-    return (doubled)
+    if a_dictionary:
+        doubled = a_dictionary.copy()
+        for a in doubled:
+            doubled[a] *= 2
+        return (doubled)
