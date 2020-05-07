154b9140d959fe6f34c5451d4fd2abffeb6997ec
Corbin Enterline <1692@holbertonschool.com>
2020-05-07 19:19:24 +0000

    Revert

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) != str or roman_string == None:
+    if type(roman_string) != str or not roman_string:
         return (0)
     b = 0
     for i in range(len(roman_string)):
@@ -42,4 +42,4 @@ def roman_to_int(roman_string):
                 b += 900
             else:
                 b += 1000
-    return (b)
+    return (int(b)
