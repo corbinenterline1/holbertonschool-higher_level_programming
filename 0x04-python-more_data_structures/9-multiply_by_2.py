2f0f4cb04cfd39fe58e5d8f60bf500d89896699d
Corbin Enterline <1692@holbertonschool.com>
2020-05-07 17:52:36 +0000

    Abs edge attempt

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,5 +3,7 @@ def multiply_by_2(a_dictionary):
     if a_dictionary:
         doubled = a_dictionary.copy()
         for a in doubled:
+            abs(doubled[a])
             doubled[a] *= 2
         return (doubled)
+
