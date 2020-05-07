193fd7befce82794619e5c29569b67c25654aa1f
Corbin Enterline <1692@holbertonschool.com>
2020-05-07 17:54:35 +0000

    Revert edge attempt

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,7 +3,8 @@ def multiply_by_2(a_dictionary):
     if a_dictionary:
         doubled = a_dictionary.copy()
         for a in doubled:
-            abs(doubled[a])
             doubled[a] *= 2
         return (doubled)
+    else:
+        return (a_dictionary.copy())
 
