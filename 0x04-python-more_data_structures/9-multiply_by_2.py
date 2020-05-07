0e9e3696773a2e2b6c70fea0ee5455fd01763fa6
Corbin Enterline <1692@holbertonschool.com>
2020-05-07 19:41:46 +0000

    pep8

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    if a_dictionary:
-        doubled = a_dictionary.copy()
-        for a in doubled:
-            doubled[a] *= 2
-        return (doubled)
-    else:
-        return (a_dictionary.copy())
-
+    doubled = a_dictionary.copy()
+    for a in doubled:
+        doubled[a] *= 2
+    return (doubled)
