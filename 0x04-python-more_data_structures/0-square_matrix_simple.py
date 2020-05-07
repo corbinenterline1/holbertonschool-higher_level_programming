03102c429c2ce24b81064c78e1a9b8153233b909
Corbin Enterline <1692@holbertonschool.com>
2020-05-07 06:44:37 +0000

    Simplified, zen of python

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,9 +2,5 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for a in range(len(matrix)):
-            new_matrix.append(square_list(matrix[a]))
+            new_matrix.append([i ** 2 for i in matrix[a]])
     return (new_matrix)
-
-
-def square_list(list):
-    return [i ** 2 for i in list]
