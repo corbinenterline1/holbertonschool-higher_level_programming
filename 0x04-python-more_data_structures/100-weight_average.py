c344827d2808b1bf807c10dd11ed9b1b23d6a8f0
Corbin Enterline <1692@holbertonschool.com>
2020-05-07 17:42:41 +0000

    Try again

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if len(my_list) == 0:
-        return (0);
+        return (0)
     scorelist = [(x * y) for x, y in my_list]
     score = sum(scorelist)
     weightlist = [i[1] for i in my_list]
