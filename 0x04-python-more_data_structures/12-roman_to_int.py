3225eac069619ab026ef69429274f1db2e7f0e64
Corbin Enterline <1692@holbertonschool.com>
2020-05-07 18:47:19 +0000

    Edge cases

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if type(roman_string) != str or not roman_string:
+        return (0)
     b = 0
     for i in range(len(roman_string)):
         if roman_string[i] == 'I':
