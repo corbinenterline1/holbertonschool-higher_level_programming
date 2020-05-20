1cf1d17d62d8f65417aa5f4fab3b889a2e65bb4e
Corbin Enterline <1692@holbertonschool.com>
2020-05-20 05:14:01 +0000

    gettin points

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+"""Class Square"""
+
+
+class Square:
+    """class Square with a private attribute size"""
+
+    def __init__(self, __size=0):
+        """Initialization method with optional size parameter, with check
+        for integer
+
+        Args:
+            self: calling it's class
+            size: size of square, default is 0
+            """
+        if type(__size) is not int:
+            raise TypeError("size must be an integer")
+        elif __size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = __size
+
+    def area(self):
+        """Returns the area of the current square
+
+        Args:
+            self: calling it's class
+            """
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        """Returns the size of the current square
+
+        Args:
+            self: calling it's class
+            """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Setter for size attribute
+
+        Args:
+            self: calling it's class
+            value: new size of square
+            """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Returns the position
+
+        Args:
+            self: calling it's class
+            """
+        return(self.__position)
+
+    @position.setter
+    def position(self, value):
+        """Setter for position attribute
+
+        Args:
+            self: calling it's class
+            value: new position
+            """
+        if type(value) is 
+
+
+    def my_print(self):
+        """Prints a square using #, with defined size for x & y dimensions
+
+        Args:
+            self: calling it's class
+            """
+        if (self.__size == 0):
+            print()
+            return
+        for y in range(self.__size):
+            for x in range(self.__size):
+                print("#", end='')
+            print()
