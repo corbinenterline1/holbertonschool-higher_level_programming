4ecd3dbaf625f64639bf9924ff2368d63f996cdf
Corbin Enterline <1692@holbertonschool.com>
2020-05-23 04:40:52 +0000

    First push ,working on tests

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Text indentation module."""
+
+
+def text_indentation(text):
+    """
+    Indents two newlines after any . ? or :
+    text must be a string, otherwise TypeError
+    There should be no space at the beg. or end of each printed line
+
+    Args:
+        text: text to be indented
+        """
+    if text is None:
+        raise TypeError("text must be a string")
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    i = 0
+    newtext = None
+
+    for i in range(len(text)):
+        if text[i - 1] == '.' or text[i - 1] == '?' or text[i - 1] == ':':
+            continue
+        if text[i] == '.' or text[i] == '?' or text[i] == ':':
+            print(text[i])
+            print()
+            continue
+        else:
+            print(text[i], end='')
