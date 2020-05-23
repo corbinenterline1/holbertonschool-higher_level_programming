7e483574c9b56703fd6c4b6fc2be07af0cf7efa9
Corbin Enterline <1692@holbertonschool.com>
2020-05-23 01:28:23 +0000

    Cleaned up documentation

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,7 +8,12 @@ def add_integer(a, b=98):
     a and b must be integers or floats, otherwise raise a TypeError exception
     a and b must be first casted to integers if they are float
     Returns an integer: the addition of a and b
+
+    Args:
+        a: first integer or float
+        b: 2nd int or float, defaults at 98
     """
+
     if type(a) not in [float, int]:
         raise TypeError("a must be an integer")
     if type(b) not in [float, int]:
