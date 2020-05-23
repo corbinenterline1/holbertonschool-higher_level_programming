5ae785b1922c63984943a4b42ab2762d076a114d
Corbin Enterline <1692@holbertonschool.com>
2020-05-23 01:16:43 +0000

    First push 0, need more tests

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Addition module: a + b"""
+
+
+def add_integer(a, b=98):
+    """
+    Addition function.
+    a and b must be integers or floats, otherwise raise a TypeError exception
+    a and b must be first casted to integers if they are float
+    Returns an integer: the addition of a and b
+    """
+    if type(a) not in [float, int]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [float, int]:
+        raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return (a + b)
