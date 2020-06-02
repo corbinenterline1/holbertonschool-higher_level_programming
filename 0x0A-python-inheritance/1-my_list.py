8dc96262120e1117c3e55791dc04a23f29bf9581
Corbin Enterline <corbinenterline1@gmail.com>
2020-06-02 23:09:18 +0000

    varread

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module for class MyList"""
+
+
+class MyList(list):
+    """MyList class. Currently only contains public instance method
+    print_sorted, which prints the sorted list.
+
+    Args:
+        list: parent to inherit from
+        """
+    def print_sorted(self):
+        """Method to print list that has been sorted.
+
+        Args:
+            self: self
+            """
+        print(sorted(self))
