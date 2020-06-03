47e57cdcdd17b9e30579ccb63f323dc1ac101711
Corbin Enterline <corbinenterline1@gmail.com>
2020-06-03 04:37:27 +0000

    varread

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -11,3 +11,4 @@ class Square(__import__('9-rectangle').Rectangle):
         """Initialization method for Square class. Size is validated
         by integer_validator method inherited from Rectangle's inheritance."""
         super().__init__(size, size)
+        self.__size = size
