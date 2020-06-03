7b292213b900c137dd085e15573186eb5ef31630
Corbin Enterline <corbinenterline1@gmail.com>
2020-06-03 03:17:09 +0000

    varread

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""0x0A 7 - Integer validator"""
+
+
+class BaseGeometry:
+    """BaseGeometry Class. Contains public instance
+    methods area(returns area) and integer_validator that validates value."""
+
+    def area(self):
+        """area function, currently poop.
+
+        Args:
+            self: self
+            """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value.
+        If value is not an integer, raises TypeError.
+        If value is less than or equal to 0, raises ValueError.
+
+        Args:
+            self: self
+            name: assumed to always be a string
+            value: value to be validated.
+            """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
