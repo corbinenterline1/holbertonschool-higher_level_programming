f2da9cd67bc97a4748c0d5fc86f3aa0db96304b5
Corbin Enterline <corbinenterline1@gmail.com>
2020-06-03 10:52:56 +0000

    varread

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 
 
 def read_file(filename=""):
+    """read file function. Prints file one line at a time."""
     with open(filename) as f:
         for line in f:
             print(line, end='')
