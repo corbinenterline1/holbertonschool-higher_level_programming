7aca5a99813c3863c5592872da6a92c9ddbbab8b
Corbin Enterline <corbinenterline1@gmail.com>
2020-06-03 00:59:10 +0000

    varread

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module for is_same_class function"""
+
+
+def is_same_class(obj, a_class):
+    """Function to check class of object against a class.
+    Returns True if the object is exactly an instance of the
+    specified class; otherwise False. Pythonic-ized!
+
+    Args:
+        obj: Object to compare
+        a_class: Class to compare object to.
+        """
+    return (type(obj) == a_class)
