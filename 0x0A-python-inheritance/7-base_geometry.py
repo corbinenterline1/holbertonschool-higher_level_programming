4d1584c352b00ed71240a037a217b84721a84f7d
Corbin Enterline <corbinenterline1@gmail.com>
2020-06-03 07:48:09 +0000

    varread

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -24,7 +24,7 @@ class BaseGeometry:
             name: assumed to always be a string
             value: value to be validated.
             """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
