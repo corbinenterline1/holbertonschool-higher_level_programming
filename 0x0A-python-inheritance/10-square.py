994d7d707cc1477de5768527fca3b7f95b59b45c
Corbin Enterline <corbinenterline1@gmail.com>
2020-06-03 04:36:10 +0000

    varread

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""0x0A - 10 - Square #1"""
+
+
+class Square(__import__('9-rectangle').Rectangle):
+    """Square class. Inherits Rectangle class,
+    which inherits BaseGeometry class. Classception. Can't wait
+    to start game programming. Got unity recently."""
+
+    def __init__(self, size):
+        """Initialization method for Square class. Size is validated
+        by integer_validator method inherited from Rectangle's inheritance."""
+        super().__init__(size, size)
