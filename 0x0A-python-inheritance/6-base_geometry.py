0c5fcbe2e3f11741aed268506419006c90d747f3
Corbin Enterline <corbinenterline1@gmail.com>
2020-06-03 02:24:34 +0000

    varread

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module for class BaseGeometry """
+
+
+class BaseGeometry:
+    """BaseGeometry Class. Contains method area."""
+
+    def area(self):
+        """area function, currently poop.
+
+        Args:
+            self: self
+            """
+        raise Exception("area() is not implemented")
