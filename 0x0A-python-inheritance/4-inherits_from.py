c49580a7a7bea54662de70c4b6e6b6b812c76ffd
Corbin Enterline <corbinenterline1@gmail.com>
2020-06-03 01:46:46 +0000

    varread

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module for checking sub class of function """
+
+
+def inherits_from(obj, a_class):
+    """ inherits_from function returns True if the object is an instance
+    of a class that inherited (directly or indirectly) from the specified
+    class; otherwise False.
+
+    Args:
+        obj: Object to compare
+        a_class: Class to compare object to
+        """
+    if (issubclass(obj.__class__, a_class) and type(obj) != a_class):
+        return True
+    else:
+        return False
