e74dece730a7df760a93d1646b7f42e2f293df16
Corbin Enterline <corbinenterline1@gmail.com>
2020-06-03 01:24:10 +0000

    varread

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module to check class or inherit from """
+
+
+def is_kind_of_class(obj, a_class):
+    """Function to check if the object is an instance of,
+    or if the object is an instance of a class that inherited from,
+    the specified class; otherwise False.
+
+    Args:
+        obj: Object to compare
+        a_class: Class toc ompare object to
+        """
+    if type(obj) == a_class or isinstance(obj, a_class):
+        return True
+    else:
+        return False
