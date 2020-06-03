9d562b4678192c36267abdae7613e9193a918690
Corbin Enterline <corbinenterline1@gmail.com>
2020-06-03 13:22:41 +0000

    varread

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+"""0x0B - 5 - To JSON string"""
+
+
+def to_json_string(my_obj):
+    """to_json_string returns the JSON representation of an object (string)
+    """
+    robj = json.dumps(my_obj)
+    return robj
