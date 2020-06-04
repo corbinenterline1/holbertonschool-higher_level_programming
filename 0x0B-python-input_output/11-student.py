c6fc94001a91bcde07a177539e8acb5b46df4a10
Corbin Enterline <corbinenterline1@gmail.com>
2020-06-04 01:51:07 +0000

    varread

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""0x0B - 11 - Student to JSON"""
+
+
+class Student:
+    """Student class. Currently contains
+    public instance attributes:
+    first_name
+    last_name
+    age
+
+    init present, to_json retrieves a dictionary representation
+    of a Student instance.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Initialization for Student class.
+
+        Args:
+            self
+            first_name
+            last_name
+            age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """class_to_json returns the dictionary description with simple
+        data structure (list, dictionary, string, integer & boolean)
+        for JSON serialization of an object.
+        Args:
+        obj: object whose dict we need (phrasing lol)
+        """
+        return obj.__dict__
