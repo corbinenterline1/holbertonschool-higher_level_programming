bf75109fa587de88604c9abbd30e57777c21723a
Corbin Enterline <corbinenterline1@gmail.com>
2020-06-10 04:48:02 +0000

    varread

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,8 +3,41 @@
 Base class testing module"""
 import unittest
 from models.base import Base
+import pep8
 
 
-class TestdaBase(unittest.TestCase):
+class TestBase(unittest.TestCase):
     """Base tests."""
 
+    def test_docs_and_pep(self):
+        """Tests for proper docstrings and PEP8 formatting."""
+        self.assertIsNotNone(Base.__doc__)
+        self.assertIsNotNone(("models.base").__doc__)
+        self.assertisNotNone(Base.__init__.__doc__)
+        self.assertisNotNone(Base.to_json_string.__doc__)
+        self.assertisNotNone(Base.save_to_file.__doc__)
+        self.assertisNotNone(Base.from_json_string.__doc__)
+        self.assertisNotNone(Base.create.__doc__)
+        self.assertisNotNone(Base.load_from_file.__doc__)
+        peppy = pep8.StyleGuide(quiet=True)
+        peppa = peppy.check_files(['models/base.py'])
+        self.assertEqual(peppa.total_errors, 0)
+
+    def test_creation(self):
+        """Creates base class, confirms it exists. Checks its' id.
+        Checks custom id, then id 2. Same 3 checks for each.
+        Makes a couple bad attributes for later tests."""
+        self.base1 = Base()
+        self.assertIsNot(self.base1, None)
+        self.assertisInstance(self.base, Base)
+        self.assertTrue(type(self.base) is Base)
+        self.assertEqual(self.base1.id, 1)
+        self.basemmb = Base(737)
+        self.assertEqual(self.basemmb.id, 737)
+        self.base2 = Base()
+        self.assertEqual(self.base2.id, 2)
+        self.baseneg = Base(-737)
+        self.basebomb = Base("bomb")
+
+if __name__ == '__main__':
+    unittest.main()
