302edc7086ba49ba95a3dca23ea72379bd0550bb
Corbin Enterline <corbinenterline1@gmail.com>
2020-07-14 00:35:38 +0000

    varread

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,5 +1,5 @@
 -- lists all the tables of a database
 -- database name passed as arg of mysql command
-SHOW TABLES;-- lists all the tables of a database
--- database name passed as arg of mysql command
 SHOW TABLES;
+
+
