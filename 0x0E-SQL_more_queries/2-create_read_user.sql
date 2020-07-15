3ac8e8e1326fd984cc95b903627dc16ffa5fccc5
Corbin Enterline <corbinenterline1@gmail.com>
2020-07-15 01:33:14 +0000

    varread

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- creates database hbtn_0d_2 & user user_0d_2
+-- user should only have select privilege
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
