5d21f9e829ac86f39b8f4ee19db70df215d97be3
Corbin Enterline <corbinenterline1@gmail.com>
2020-07-15 04:53:49 +0000

    varread

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- list all genres from hbtn_0d_tvshows & display # of shows linked to each
 -- Record displays: <TV Show genre> - <Number of shows linked to this genre>
 -- first column called genre, second number_of_shows. No genres with no shows
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres JOIN tv_show_genres WHERE tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_show_genres.genre_id ORDER BY number_shows DESC;
