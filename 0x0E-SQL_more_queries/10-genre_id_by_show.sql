791e628467f8469453e90cab3763641e95aaa8e8
Corbin Enterline <corbinenterline1@gmail.com>
2020-07-15 04:13:25 +0000

    varread

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,4 +3,4 @@
 -- Ascending order by tv_shows.titles & tv_shows_genre.genre_id. 1 SELECT
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
