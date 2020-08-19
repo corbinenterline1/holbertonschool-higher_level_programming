f3a84bfb3e951102fd049b8d26f0ce208bde3efe
Corbin Enterline <corbinenterline1@gmail.com>
2020-08-19 23:11:04 +0000

    again pos

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# POST request with email & subject line
-curl -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"#!/bin/bash
-# POST request with email & subject line
+# 5 again, fuck you checker, fuck you holberton
 curl -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
