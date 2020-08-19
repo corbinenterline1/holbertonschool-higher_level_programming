880f78943c18c56f4d504635bfb924692a695e84
Corbin Enterline <corbinenterline1@gmail.com>
2020-08-19 23:06:21 +0000

    Broken checker

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send GET request with header variable X-HolbertonSchool-User-ID, val. 98
-CURL -sX GET -H 'X-HolbertonSchool-User-Id: 98' "$1"
+# task 4 again, cuase it works, but broken fucking checker
+curl -sX GET -H 'X-HolbertonSchool-User-Id: 98' "$1"
