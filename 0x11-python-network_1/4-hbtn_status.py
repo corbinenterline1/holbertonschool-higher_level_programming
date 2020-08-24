7bcaf08f5bff4abc03075cb6b00e2a0699e95c1f
Corbin Enterline <corbinenterline1@gmail.com>
2020-08-24 18:39:48 +0000

    4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""0x11 - 4"""
+import requests
+
+
+if __name__ == "__main__":
+        getit = requests.get('https://intranet.hbtn.io/status').text
+        print('Body response:')
+        print('\t- type {}'.format(type(getit)))
+        print('\t- content: {}'.format(getit))
