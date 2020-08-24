782e6d1250acf569249cba93b0ef7fcf5450ba9f
Corbin Enterline <corbinenterline1@gmail.com>
2020-08-24 20:39:00 +0000

    stringify

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,8 +4,8 @@ import requests
 import sys
 
 
-if __name__ == "__main__":
-    dats = {"email": sys.argv[2]}
+if __name__ == '__main__':
+    dats = {'email': str(sys.argv[2])}
     try:
         getit = requests.get(sys.argv[1], data=dats)
         print(getit.text)
