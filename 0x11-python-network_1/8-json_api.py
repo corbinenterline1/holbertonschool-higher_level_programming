a3a501fc9ce85249cffae50c3f80e532264d4eae
Corbin Enterline <corbinenterline1@gmail.com>
2020-08-25 01:43:23 +0000

    format and isset gone

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,19 +4,17 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    if isset(sys.argv[1]):
+    try:
         q = sys.argv[1]
-    else:
+    except:
         q = ""
     response = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
     jasponse = {}
     try:
         jaspons = response.json()
         if len(jaspons) > 0:
-            print("[{:d}] {}."format(jaspons.get("id"), jaspons.get("name")))
+            print("[{:d}] {}".format(jaspons.get("id"), jaspons.get("name")))
         else:
             print("No result")
     except:
         print("Not a valid JSON")
-
-
