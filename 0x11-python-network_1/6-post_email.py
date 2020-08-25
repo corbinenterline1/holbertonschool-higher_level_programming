89605e05d570130866734ab854e29b013de59631
Corbin Enterline <corbinenterline1@gmail.com>
2020-08-25 01:33:06 +0000

    no wonder it only worked on container

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,5 +6,5 @@ import sys
 
 if __name__ == '__main__':
     dats = {'email': str(sys.argv[2])}
-    getit = requests.get(sys.argv[1], data=dats)
+    getit = requests.post(sys.argv[1], data=dats)
     print(getit.text)
