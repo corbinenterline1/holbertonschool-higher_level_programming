8ad03c530283987e1a4af53c9cb4250c1a6b70dd
Corbin Enterline <corbinenterline1@gmail.com>
2020-09-16 03:46:35 +0000

    11 lazy way

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Searches for second biggest integer in list of args
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const args = process.argv.slice(2);
+  args.sort(function (a, b) { return b - a; });
+  console.log(args);
+  console.log(args[1]);
+}
