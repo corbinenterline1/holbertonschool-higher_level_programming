c84457ba4b2dffb3cbb34cbdbbca51a816ac33fc
Corbin Enterline <corbinenterline1@gmail.com>
2020-09-16 03:54:31 +0000

    left print in

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 // Searches for second biggest integer in list of args
-if (process.argv.length <= 2) {
+if (process.argv.length <= 3) {
   console.log(0);
 } else {
   const args = process.argv.slice(2);
   args.sort(function (a, b) { return b - a; });
-  console.log(args);
   console.log(args[1]);
 }
