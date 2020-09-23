3004a81fc1adc1eaf5a136af173b4d3052cc8aa0
Corbin Enterline <corbinenterline1@gmail.com>
2020-09-23 04:09:18 +0000

    semistandard

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//WEDGE ANTILLES, IT'S YOUR TIME
+// WEDGE ANTILLES, IT'S YOUR TIME
 const request = require('request');
 request(process.argv[2], function (err, response, body) {
   if (err) {
